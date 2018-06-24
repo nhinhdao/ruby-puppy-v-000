8b5574026fb8fec2864824bd81f3d703d380e8b6
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-24 16:18:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+    @@all = []
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.all
+        @@all.each {|e| puts e.name}
+    end
+end
